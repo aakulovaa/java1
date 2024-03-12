e62eadb08c06b881a367c01b98aea4ddd71d7fd2
annakulova <aakulovaa03@gmail.com>
2024-03-12 14:39:32 +0300

    Second commit
    Создание класса Контейнер с свойствами и конструктором для создания нового объекта

---
+++ src/main/java/org/example/Container.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Container {
+    private int[] arrayForContainer;
+    private int containerSize;
+    public Container() {
+        arrayForContainer = new int[1];
+        containerSize = 0;
+    }
+}
