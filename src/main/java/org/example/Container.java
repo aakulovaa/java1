fc53a49fed338b05baa5664baf2fd1f3d72ce7c8
annakulova <aakulovaa03@gmail.com>
2024-03-12 16:08:53 +0300

    javadoc

--- src/main/java/org/example/Container.java
+++ src/main/java/org/example/Container.java
@@ -1,14 +1,27 @@
 package org.example;
 
+/** Класс Контейнер со свойствами <b>arrayForContainer</b> и <b>containerSize</b>.
+ * @autor Анна Акулова
+ */
 public class Container {
+    /** Поле массив для контейнера */
     private int[] arrayForContainer;
+    /** Поле размерность контейнера */
     private int containerSize;
 
+    /**
+     * Конструктор - создание нового объекта
+     * @see Container#Container()
+     */
     public Container() {
         arrayForContainer = new int[1];
         containerSize = 0;
     }
 
+    /**
+     * Метод добавления элемента в конец массива
+     * @param element - добавляемый элемент
+     */
     public void addElement(int element)
     {
         int[] longerArray = new int[arrayForContainer.length + 1];
@@ -23,10 +36,20 @@ public class Container {
         containerSize++;
     }
 
+    /**
+     * Метод получения элемента по индексу
+     * @param index - индекс извлекаемого элемента
+     * @return возвращает элемент массива по заданному индексу
+     */
     public int getElement(int index) {
         return arrayForContainer[index];
     }
 
+    /**
+     * Метод добавления элемента по индексу
+     * @param index - индекс добавляемого элемента
+     * @param element - элемент для добавления
+     */
     public void addElementForIndex(int index, int element)
     {
         int[] longerArray = new int[arrayForContainer.length + 1];
@@ -45,6 +68,11 @@ public class Container {
         arrayForContainer = longerArray;
     }
 
+    /**
+     * Метод удаления элемента по индексу
+     * @param index - индекс удаляемого элемента
+     * @return возвращает удаленный элемент
+     */
     public int removeElementForIndex(int index)
     {
         int removedElement = getElement(index);
