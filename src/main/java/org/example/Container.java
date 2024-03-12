f0861611a164b37af3fcd2f4e87f9d1a0f4b897d
annakulova <aakulovaa03@gmail.com>
2024-03-12 15:12:03 +0300

    Create method Add
    Создание метода добавления элемента в конец массива.

--- src/main/java/org/example/Container.java
+++ src/main/java/org/example/Container.java
@@ -3,8 +3,23 @@ package org.example;
 public class Container {
     private int[] arrayForContainer;
     private int containerSize;
+
     public Container() {
         arrayForContainer = new int[1];
         containerSize = 0;
+    }
+
+    public void addElement(int element)
+    {
+        int[] longerArray = new int[arrayForContainer.length + 1];
+        for(int i = 0; i < arrayForContainer.length; i++)
+        {
+            longerArray[i] = arrayForContainer[i];
+
+        }
+        arrayForContainer = longerArray;
+        longerArray[containerSize] = element;
+
+        containerSize++;
     }
 }
