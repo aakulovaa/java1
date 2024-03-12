abe454110f8756a3de26a85719365d80b67f0de6
annakulova <aakulovaa03@gmail.com>
2024-03-12 16:03:19 +0300

    Commit for main

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,23 +1,44 @@
 package org.example;
+import java.util.Scanner;
 
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
         Container container = new Container();
-        container.addElement(7);
-        container.addElement(1);
-        container.addElement(3);
-        container.addElementForIndex(3,5);
-        container.addElementForIndex(4,9);
-        container.addElementForIndex(5,8);
-        System.out.println(container.removeElementForIndex(0));
-        System.out.println(container.getElement(0));
-        System.out.println(container.getElement(1));
-        System.out.println(container.getElement(2));
-        System.out.println(container.getElement(3));
-        System.out.println(container.getElement(4));
-        System.out.println(container.getElement(5));
+        Scanner scanner = new Scanner(System.in);
+        boolean flagExit = true;
+        int choice;
+        while (flagExit) {
+            System.out.println("Выберите действие: 1-добавить элемент в конец. 2-добавить элемент по индексу." +
+                    "3-получить значение элемента по индексу. 4-удалить элемент по индексу. 5 - выход ");
+            choice = scanner.nextInt();
+            switch (choice) {
+                case (1):
+                    System.out.println("Введите элемент");
+                    int element = scanner.nextInt();
+                    container.addElement(element);
+                    break;
+                case (2):
+                    System.out.println("Введите индекс и элемент");
+                    int index = scanner.nextInt();
+                    int elementForAdd = scanner.nextInt();
+                    container.addElementForIndex(index, elementForAdd);
+                    break;
+                case (3):
+                    System.out.println("Введите индекс элемента");
+                    int indexForGet = scanner.nextInt();
+                    System.out.println(container.getElement(indexForGet));
+                    break;
+                case (4):
+                    System.out.println("Введите индекс элемента");
+                    int indexForRemove = scanner.nextInt();
+                    System.out.println(container.removeElementForIndex(indexForRemove));
+                    break;
+                case (5):
+                    flagExit = false;
+                    break;
+            }
 
+        }
     }
 }
